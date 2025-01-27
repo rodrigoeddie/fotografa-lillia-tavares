d4d2034e162d3c86ffc70dd164f5320bc298279f
Rodrigo de Melo <rodrigodemelo@luego.com.br>
2025-01-27 14:14:19 -0300

    Working...

--- pages/trabalhos/[slug]/index.vue
+++ pages/trabalhos/[slug]/index.vue
@@ -1,20 +1,122 @@
-<script lang="ts" setup>
-// useSeoMeta({
-//   title: ensaio.seo.title,
-//   ogTitle: ensaio.seo.title,
-//   description: ensaio.seo.description,
-//   ogDescription: ensaio.seo.description,
-//   ogImage: ensaio.seo.image,
-//   twitterCard: ensaio.seo.image,
-// })
+<script setup lang="ts">
+const path = useRoute().path
+
+const { data: work } = await useAsyncData(path, () => {
+  return queryCollection('works').path(path).first()
+})
+
+const highlight = work.value.body.album.filter(item => item.highlight);
 </script>
 
 <template>
-  <h1 class="title">ENSAIO</h1>
+  <div class="container no-padding">
+    <div class="wrap-hero">
+      <div class="text">
+        <div class="about-text text-slide">
+          <h2 class="title" v-html="work.title"></h2>
+          <div class="description" v-html="work.description"></div>
+        </div>
+      </div>
+
+      <div :class="{'has-two': highlight[1]}" class="wrap-img-hero">
+        <nuxt-img
+          :src='"https://imagedelivery.net/oEk64Oj9wn0qdlDuKEONYg/" + highlight[0].uri + "/public"'
+          width="1920"
+          loading="lazy"/>
+        <nuxt-img
+          v-if="highlight[1]"
+          :src='"https://imagedelivery.net/oEk64Oj9wn0qdlDuKEONYg/" + highlight[1].uri + "/public"'
+          width="1920"
+          loading="lazy"/>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-.title {
-  color: black;
+<style scoped lang="scss">
+.wrap-hero {
+  display: flex;
+
+  .wrap-img-hero {
+    flex-shrink: 0;
+    display: flex;
+    width: 55%;
+    
+    &.has-two {
+      height: 80dvh;
+      width: auto;
+
+      img {
+        flex-shrink: 0;
+        height: 100%;
+        width: auto;
+      }
+    }
+  }
+
+  .text {
+    justify-content: flex-end;
+    flex-direction: column;
+    background: white;
+    display: flex;
+    width: 45%;
+  }
+
+  .about-text {
+    padding: 180rem v.$space v.$space;
+
+    .description {
+      padding-top: 0;
+
+      p {
+        padding-top: 15rem !important;
+      }
+
+      strong,
+      b {
+        background: #892c1a;
+        color: white;
+        padding-right: 3rem;
+        padding-left: 3rem;
+      }
+    }
+
+    &.text-slide {
+      .title {
+        padding-top: 20rem;
+        padding-bottom: 30rem;
+      }
+
+      .description {
+        padding-bottom: 20rem;
+
+        p {
+          padding-top: 0 !important;
+        }
+      }
+    }
+  }
+
+  .about-ctas {
+    justify-content: center;
+    padding: 20rem v.$space;
+    background: #892c1a;
+    align-items: center;
+    height: 150rem;
+    display: flex;
+
+    .btn {
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      left: 0;
+      top: 0;
+
+      &:hover {
+        background-color: #6d1d0b !important;
+        color: white;
+      }
+    }
+  }
 }
 </style>
