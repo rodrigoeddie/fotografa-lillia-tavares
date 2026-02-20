39fa8a6828f2d351eae52ebd5aafb0ab0802518a
Rodrigo de Melo Lima <rodrigodemelo@luego.com.br>
2026-02-20 20:22:04 -0300

    Working...

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,50 @@
       url: 'https://fotografalilliatavares.com.br'
     })
   ]);
+
+  // Scripts de terceiros otimizados
+  useScript({
+    key: 'facebook-pixel',
+    src: 'https://connect.facebook.net/en_US/fbevents.js',
+    async: true,
+  }, {
+    stub: ({ fn }) => {
+      // Inicializa o stub do Facebook Pixel
+      // @ts-ignore
+      window.fbq = fn;
+      // @ts-ignore
+      window.fbq.queue = [];
+    },
+    use() {
+      // @ts-ignore
+      if (typeof window.fbq !== 'undefined') {
+        // @ts-ignore
+        window.fbq('init', '4335671090015485');
+        // @ts-ignore
+        window.fbq('track', 'PageView');
+      }
+    }
+  });
+
+  useScript({
+    key: 'smartlook',
+    src: 'https://web-sdk.smartlook.com/recorder.js',
+    async: true,
+  }, {
+    stub: ({ fn }) => {
+      // @ts-ignore
+      window.smartlook = fn;
+      // @ts-ignore
+      window.smartlook.api = [];
+    },
+    use() {
+      // @ts-ignore
+      if (typeof window.smartlook !== 'undefined') {
+        // @ts-ignore
+        window.smartlook('init', '03d8d5209060cf01f72fb068bb5405e907929694', { region: 'eu' });
+      }
+    }
+  });
 </script>
 <template>
   <div class="wrapper-main">
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -46,37 +46,6 @@ export default defineNuxtConfig({
           content: 'https://www.fotografalilliatavares.com.br/share.webp?v3',
         },
       ],
-      script: [
-        {
-          type: 'text/javascript',
-          innerHTML: `
-            window.smartlook||(function(d) {
-              var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
-              var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
-              c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
-            })(document);
-            smartlook('init', '03d8d5209060cf01f72fb068bb5405e907929694', { region: 'eu' });
-          `,
-        },
-        {
-          type: 'text/javascript',
-          innerHTML: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '4335671090015485');
-            fbq('track', 'PageView');
-          `,
-        },
-      ],
-      __dangerouslyDisableSanitizersByTagID: {
-        'smartlook-script': ['innerHTML']
-      },
       link: [
         {
           rel: 'preconnect',
