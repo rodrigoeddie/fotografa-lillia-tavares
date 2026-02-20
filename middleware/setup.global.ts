5d5e31c32f57ba85064f985299047abb3c876c7d
Rodrigo de Melo Lima <rodrigodemelo@luego.com.br>
2026-02-20 19:50:14 -0300

    Working...

--- middleware/setup.global.ts
+++ middleware/setup.global.ts
@@ -13,6 +13,8 @@ export default defineNuxtRouteMiddleware((to, from) => {
         return navigateTo('/ensaio-fotografico/sensual-intimista/cris-borges', { redirectCode: 301 })
     } else if (to.path === '/ensaios/cris-pole-e-lira') {
         return navigateTo('/ensaio-fotografico/sensual-intimista/cris-pole-e-lira', { redirectCode: 301 })
+    } else if (to.path === '/blog/fotografia-corporativa/as-vantagens-de-investir-em-fotos-corporativas-profissionais') {
+        return navigateTo('/blog/fotografia-corporativa/vantagens-fotos-corporativas', { redirectCode: 301 })
     }
 
     // if (process.client && to.path !== from.path) {
