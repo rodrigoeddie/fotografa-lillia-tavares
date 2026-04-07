fc684e8e0987f7f2a9926c49196f92d298f2dcc0
Rodrigo de Melo Lima <rodrigodemelo@luego.com.br>
2026-04-07 17:10:55 -0300

    Working...

--- pages/orcamentos/dia-das-maes/index.vue
+++ pages/orcamentos/dia-das-maes/index.vue
@@ -4,6 +4,31 @@ const imgSrcset = [
   'https://images.fotografalilliatavares.com.br/images/f7c0628e-d30e-4b09-9033-16fc0815b600/w=500,fit=scale-down 1x',
   'https://images.fotografalilliatavares.com.br/images/f7c0628e-d30e-4b09-9033-16fc0815b600/w=1000,fit=scale-down 2x',
 ].join(', ');
+const title = 'Presente de Dia das Mães em Mogi das Cruzes: Ensaio Fotográfico Especial';
+const description = 'Se você está procurando um presente de dia das mães em Mogi das Cruzes, uma das opções mais especiais é um ensaio fotográfico profissional. Diferente de presentes comuns, como flores ou roupas, as fotos se tornam lembranças para toda a vida.';
+const link = 'https://fotografalilliatavares.com.br/presente-ensaio-fotografico-mogi/dia-das-maes';
+const imageShare = '737d6560-9e6a-49b4-b503-4c7d56dad900';
+const slug = 'dia-das-maes';
+
+useSchemaOrg([
+  defineWebPage({
+    name: title,
+    url: link
+  })
+]);
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description,
+  ogUrl: link,
+  ogImage: `https://images.fotografalilliatavares.com.br/images/${imageShare}/public`,
+  twitterCard: 'summary_large_image',
+  twitterTitle: title,
+  twitterDescription: description,
+  twitterImage: `https://images.fotografalilliatavares.com.br/images/${imageShare}/public`,
+});
 
 definePageMeta({
   layout: 'empty-layout'
